a3ab2dc377048d52efab39992110b5803213138f
eeritvan <eetu.ritvanen@helsinki.fi>
2024-12-01 19:41:35 +0200

    Some testing with react-hook-form

--- client/src/routes/settings/index.tsx
+++ client/src/routes/settings/index.tsx
@@ -1,6 +1,29 @@
+import { useForm, SubmitHandler } from 'react-hook-form'
+
+
+let renderCount = 0
+
 const Settings = () => {
-  console.log('settings')
-  return <div> This is settings page </div>
+  console.log('opened settings')
+  renderCount++
+
+  const { register, handleSubmit } = useForm()
+  const onSubmit = (data: FormData) => console.log(data)
+
+  return (
+    <div>
+      This is settings page
+      <div>
+        rerenders: {renderCount}
+      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register('firstName', { minLength: 3 }) } />
+        <input {...register('lastName', { minLength: 3 }) } />
+        <input type='submit' />
+        testi
+      </form>
+    </div>
+  )
 }
 
 export default Settings
