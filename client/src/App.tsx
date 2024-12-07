179ec3d55ee53569ca9d0ee62131ed65e406eb27
eeritvan <eetu.ritvanen@helsinki.fi>
2024-12-07 19:53:07 +0200

    Added initial layout for the application

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,40 +1,43 @@
 import { lazy, Suspense } from 'react'
 import { Router, Link, Route, Switch } from 'wouter'
 
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable"
+
 const Settings = lazy(() => import('./routes/settings'))
 const Users = lazy(() => import('./routes/users'))
 const Counter = lazy(() => import('./routes/counter'))
 
 const App = () => {
   return (
-    <>
-      {/* <Router>
-       <h1 className="text-3xl font-bold underline">
-         Hello world!
-       </h1>
-      <div>
-        <Link to='/settings'>settings</Link>
-        <Link to='/users/321'>users</Link>
-        <Link to='/counter'>counter</Link>
-      </div> */}
+    <Router>
       <div className='flex h-screen w-screen'>
-        <div className='container justify-center bg-red-500 min-w-52'>
-          testi
-        </div>
-        <div className='container bg-blue-600'>
-          testi
-        </div>
+        <ResizablePanelGroup direction="horizontal">
+          <ResizablePanel className='bg-red-600 rounded-lg p-2 m-1'>
+            <Suspense fallback={<div>Loading...</div>}>
+              <Switch>
+                <Route path='/settings' component={Settings} />
+                <Route path='/users/:name' component={Users} />
+                <Route path='/counter' component={Counter} />
+                <Route> 404: No such page! </Route>
+              </Switch>
+            </Suspense>
+          </ResizablePanel>
+          <ResizableHandle className='my-3 w-1 rounded-sm' withHandle/>
+          <ResizablePanel className='bg-blue-600 rounded-lg p-2 m-1'>
+            <Link to='/settings'>settings</Link>
+            <br />
+            <Link to='/users/321'>users</Link>
+            <br />
+            <Link to='/counter'>counter</Link>
+            <br />
+          </ResizablePanel>
+        </ResizablePanelGroup>
       </div>
-      {/* <Suspense fallback={<div>Loading...</div>}>
-        <Switch>
-          <Route path='/settings' component={Settings} />
-          <Route path='/users/:name' component={Users} />
-          <Route path='/counter' component={Counter} />
-          <Route> 404: No such page! </Route>
-        </Switch>
-      </Suspense>
-    </Router> */}
-    </>
+    </Router>
   )
 }
 
