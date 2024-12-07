64f2f025f7a860ee595d4ed9cd477e932b77cbdc
eeritvan <eetu.ritvanen@helsinki.fi>
2024-12-07 17:39:09 +0200

    Added shadcn and tailwind for ui customization

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,23 +7,34 @@ const Counter = lazy(() => import('./routes/counter'))
 
 const App = () => {
   return (
-    <Router>
+    <>
+      {/* <Router>
+       <h1 className="text-3xl font-bold underline">
+         Hello world!
+       </h1>
       <div>
         <Link to='/settings'>settings</Link>
         <Link to='/users/321'>users</Link>
         <Link to='/counter'>counter</Link>
+      </div> */}
+      <div className='flex h-screen w-screen'>
+        <div className='container justify-center bg-red-500 min-w-52'>
+          testi
+        </div>
+        <div className='container bg-blue-600'>
+          testi
+        </div>
       </div>
-      <Suspense fallback={<div>Loading...</div>}>
+      {/* <Suspense fallback={<div>Loading...</div>}>
         <Switch>
           <Route path='/settings' component={Settings} />
           <Route path='/users/:name' component={Users} />
           <Route path='/counter' component={Counter} />
-          <Route>
-            404: No such page!
-          </Route>
+          <Route> 404: No such page! </Route>
         </Switch>
       </Suspense>
-    </Router>
+    </Router> */}
+    </>
   )
 }
 
