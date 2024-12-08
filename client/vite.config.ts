3614a64c3952758d59cd65b271a341c87ec19ca8
eeritvan <eetu.ritvanen@helsinki.fi>
2024-12-08 22:59:27 +0200

    Some small improvements to production builds

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,27 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({ open: true })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
+    }
+  },
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom']
+        }
+      }
     }
   }
 })
