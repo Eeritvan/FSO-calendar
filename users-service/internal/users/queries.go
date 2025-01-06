5d19222ba57ba77bc8ab360e2cdb1ad051b3c269
eeritvan <eeritvan@proton.me>
2025-01-06 20:57:07 +0200

    Added timeout for psql operations

--- users-service/internal/users/queries.go
+++ users-service/internal/users/queries.go
@@ -4,25 +4,31 @@ import (
 	"context"
 	"fmt"
 	"graphql/graph/model"
+	"time"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
 )
 
 func QueryUser(ctx context.Context, db *pgx.Conn, username string) (*model.User, error) {
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
 	var user model.User
-	err := db.QueryRow(ctx, `
+	if err := db.QueryRow(ctx, `
 		SELECT username, password_hash, COALESCE(totp, '')
 		FROM users
 		WHERE username = $1
-	`, username).Scan(&user.Username, &user.Password, &user.Totp)
-	if err != nil {
+	`, username).Scan(&user.Username, &user.Password, &user.Totp); err != nil {
 		return nil, fmt.Errorf("failed to fetch user information %v", err)
 	}
 	return &user, nil
 }
 
 func UpdateUserTotp(ctx context.Context, db *pgx.Conn, username string, totpSecret *string) error {
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
 	_, err := db.Exec(ctx, `
         UPDATE users 
         SET totp = $2
@@ -35,8 +41,11 @@ func UpdateUserTotp(ctx context.Context, db *pgx.Conn, username string, totpSecr
 }
 
 func CreateUser(ctx context.Context, db *pgx.Conn, username string, password []byte) (*model.User, error) {
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
 	var user model.User
-	err := db.QueryRow(ctx, `
+	if err := db.QueryRow(ctx, `
 		INSERT INTO users (username, password_hash)
 		VALUES ($1, $2)
 		RETURNING id, username, password_hash, totp
@@ -45,9 +54,7 @@ func CreateUser(ctx context.Context, db *pgx.Conn, username string, password []b
 		&user.Username,
 		&user.Password,
 		&user.Totp,
-	)
-
-	if err != nil {
+	); err != nil {
 		pgErr := err.(*pgconn.PgError)
 		switch pgErr.Code {
 		case "23505":
