f61b83f0e04adccad43d3d86f2b8a05e43feda04
eeritvan <eeritvan@proton.me>
2025-01-06 20:57:16 +0200

    improved validation

--- users-service/internal/users/validation.go
+++ users-service/internal/users/validation.go
@@ -1,6 +1,9 @@
 package users
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 var (
 	ErrInvalidPassword = fmt.Errorf("password must be at least 8 characters long")
@@ -8,12 +11,29 @@ var (
 	ErrUserExists      = fmt.Errorf("username already exists")
 )
 
-func ValidateUserInput(username string, password string) error {
+func ValidateUsername(username string) error {
 	if len(username) < 3 {
 		return ErrInvalidUsername
 	}
+	if strings.TrimSpace(username) == "" {
+		return ErrInvalidUsername
+	}
+	return nil
+}
+
+func ValidatePassword(password string) error {
 	if len(password) < 8 {
 		return ErrInvalidPassword
+	}
+	return nil
+}
+
+func ValidateUserInput(username string, password string) error {
+	if err := ValidateUsername(username); err != nil {
+		return err
+	}
+	if err := ValidatePassword(password); err != nil {
+		return err
 	}
 	return nil
 }
