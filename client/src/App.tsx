198c7e04d57322917e15118f670672aa80eda313
eeritvan <eeritvan@proton.me>
2025-01-09 18:15:26 +0200

    added separate login page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import SidePanel from './components/sidePanel'
 import MainView from './components/mainView'
+import Login from './components/authentication/login'
 import Split from 'react-split'
 import usePanelSizeSlice from './store/panelSizeStore'
 import { useQuery } from '@tanstack/react-query'
-import { loginMutation } from './graphql/mutations'
+import { Route } from 'wouter'
+// import { loginMutation } from './graphql/mutations'
 
 const App = () => {
   const {
@@ -13,39 +15,42 @@ const App = () => {
     setIsDragging
   } = usePanelSizeSlice()
 
-  useQuery({
-    queryKey: ['login'],
+  const { data: token } = useQuery({
+    queryKey: ['token'],
     refetchOnWindowFocus: false,
     queryFn: async () => {
       const localToken = window.localStorage.getItem('user-token')
-      if (!localToken) {
-        const data = await loginMutation
-          .send({ username: 'test', password: 'test' })
-        localStorage.setItem('user-token', JSON.stringify(data))
-      }
+      // if (!localToken) {
+      //   const data = await loginMutation
+      //     .send({ username: 'test', password: 'test' })
+      //   localStorage.setItem('user-token', JSON.stringify(data))
+      // }
       return localToken
     }
   })
 
   return (
-    <Split
-      className="flex h-screen w-screen"
-      gutterSize={5}
-      sizes={[defaultSize, 100 - defaultSize]}
-      minSize={[50, 300]}
-      maxSize={[400, Infinity]}
-      snapOffset={0}
-      dragInterval={expandedView ? Infinity : 1}
-      cursor={expandedView ? 'default' : 'col-resize'}
-      onDragStart={() => !expandedView && setIsDragging(true)}
-      onDragEnd={() => !expandedView && setIsDragging(false)}
-      onDrag={(sizes) =>
-        !expandedView && setDefaultSize(sizes[0])
-      }
-    >
-      <SidePanel />
-      <MainView />
-    </Split>
+    <>
+      {!token && <Route path='/login' component={Login} />}
+      {token && <Split
+        className="flex h-screen w-screen"
+        gutterSize={5}
+        sizes={[defaultSize, 100 - defaultSize]}
+        minSize={[50, 300]}
+        maxSize={[400, Infinity]}
+        snapOffset={0}
+        dragInterval={expandedView ? Infinity : 1}
+        cursor={expandedView ? 'default' : 'col-resize'}
+        onDragStart={() => !expandedView && setIsDragging(true)}
+        onDragEnd={() => !expandedView && setIsDragging(false)}
+        onDrag={(sizes) =>
+          !expandedView && setDefaultSize(sizes[0])
+        }
+      >
+        <SidePanel />
+        <MainView />
+      </Split>}
+    </>
   )
 }
 
---
+++ client/src/components/authentication/login/index.tsx
@@ -0,0 +1,63 @@
+import { useForm } from 'react-hook-form'
+import { valibotResolver } from '@hookform/resolvers/valibot'
+import { object, pipe, string, minLength, number } from 'valibot'
+import { useMutation } from '@tanstack/react-query'
+import { loginMutation } from '../../../graphql/mutations'
+
+interface LoginFormData {
+  username: string
+  password: string
+  totp: number
+}
+
+const schema = object({
+  username: pipe(
+    string('username is required'),
+    minLength(3, 'Needs to be at least 3 characters')
+  ),
+  password: pipe(
+    string('password is required')
+  ),
+  totp: pipe(
+    number('must be a number')
+  )
+})
+
+const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<LoginFormData>({
+    resolver: valibotResolver(schema)
+  })
+
+  const loginMutate = useMutation({
+    mutationFn: async (data: LoginFormData) => {
+      const result = await loginMutation
+        .send({ username: data.username, password: data.password })
+      window.localStorage.setItem('user-token', JSON.stringify(result))
+      return result
+    }
+  })
+
+  const onSubmit = (data: LoginFormData) => {
+    loginMutate.mutate(data)
+  }
+
+  return (
+    <>
+      Login
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register('username')} /> <br />
+        <input {...register('password')} /> <br />
+        <input {...register('totp', { valueAsNumber: true })} type='number'/>
+        <br />
+        {errors.totp && <p>{errors.totp.message}</p>}
+        <input type='submit' />
+      </form>
+    </>
+  )
+}
+
+export default Login
