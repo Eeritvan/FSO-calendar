753ab4b8c80586656a642ac23eb4b7b6ba965d8f
eeritvan <eeritvan@proton.me>
2025-01-09 23:56:54 +0200

    fixed client login errors

--- client/src/components/authentication/login/index.tsx
+++ client/src/components/authentication/login/index.tsx
@@ -1,13 +1,23 @@
 import { useForm } from 'react-hook-form'
 import { valibotResolver } from '@hookform/resolvers/valibot'
-import { object, pipe, string, minLength, number } from 'valibot'
 import { useMutation } from '@tanstack/react-query'
 import { loginQuery } from '../../../graphql/mutations'
+import { Redirect } from 'wouter'
+import {
+  object,
+  pipe,
+  string,
+  minLength,
+  number,
+  optional,
+  maxValue,
+  minValue
+} from 'valibot'
 
 interface LoginFormData {
   username: string
   password: string
-  totp: number
+  totp?: number
 }
 
 const schema = object({
@@ -18,9 +28,11 @@ const schema = object({
   password: pipe(
     string('password is required')
   ),
-  totp: pipe(
-    number('must be a number')
-  )
+  totp: optional(pipe(
+    number('must be a number'),
+    minValue(0, 'incorrect totp format'),
+    maxValue(999999, 'incorrect totp format')
+  ))
 })
 
 const Login = () => {
@@ -35,24 +47,43 @@ const Login = () => {
   const loginMutate = useMutation({
     mutationFn: async (data: LoginFormData) => {
       const result = await loginQuery
-        .send({ username: data.username, password: data.password })
+        .send({
+          username: data.username,
+          password: data.password,
+          totp: data.totp ? data.totp : null
+        })
+      if (result.errors) {
+        throw result.errors[0].message
+      }
+
       window.localStorage.setItem('user-token', JSON.stringify(result))
       return result
-    }
+    },
+    // eslint-disable-next-line no-console
+    onError: (error) => console.log(error)
   })
 
-  const onSubmit = (data: LoginFormData) => {
-    loginMutate.mutate(data)
-  }
+  const onSubmit = async (data: LoginFormData) => loginMutate.mutate(data)
 
   return (
     <>
       Login
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register('username')} /> <br />
-        <input {...register('password')} /> <br />
-        <input {...register('totp', { valueAsNumber: true })} type='number'/>
-        <br />
+        <input {...register('username')}
+          placeholder="username"
+        /> <br />
+        <input {...register('password')}
+          type="password"
+          placeholder="password"
+        /> <br />
+        <input {...register('totp', {
+          setValueAs: (value: string) => {
+            return value === '' ? undefined : Number(value)
+          }
+        })}
+        type='number'
+        placeholder="6-digit TOTP"
+        /> <br />
         {errors.totp && <p>{errors.totp.message}</p>}
         <input type='submit' />
       </form>
--- client/src/graphql/mutations.ts
+++ client/src/graphql/mutations.ts
@@ -1,14 +1,15 @@
 import { Graffle } from 'graffle'
 
-const graffle = Graffle.create().transport({
-  url: 'http://localhost:8080/query'
-})
+const graffle = Graffle
+  .create({ output: { envelope: { errors: { execution: true } } } })
+  .transport({ url: 'http://localhost:8080/query' })
 
 export const loginQuery = graffle.gql(`
-  mutation Login($username: String!, $password: String!) {
+  mutation Login($username: String!, $password: String!, $totp: String) {
     login(input: {
       username: $username,
-      password: $password
+      password: $password,
+      totp: $totp 
     }) {
       username
       token
