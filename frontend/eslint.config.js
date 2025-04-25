52233363d1c204246cb8ee8c9288fda8f4b9c86c
eeritvan <eeritvan@proton.me>
2025-04-26 00:17:45 +0300

    feat: added react compiler

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,22 +3,42 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
 
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-    plugins: { js },
-    extends: ["js/recommended"] },
-  {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-    languageOptions:{ globals: globals.browser }
+    languageOptions:{
+      ecmaVersion: "latest",
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    }
   },
+
+  js.configs.recommended,
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  reactHooks.configs.recommended,
+  reactRefresh.configs.recommended,
 
   { ignores: ["build", ".react-router"] },
   {
+    settings: {
+      react: {
+        version: "detect"
+      }
+    },
     rules: {
+      "react-hooks/react-compiler": ["error"],
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true }
+      ],
       semi: ["error", "always"],
       quotes: ["error", "double"],
       indent: ["error", 2],
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,22 @@
+import react from "@vitejs/plugin-react";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const ReactCompilerConfig = { target: "19" };
+
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          ["babel-plugin-react-compiler", ReactCompilerConfig]
+        ]
+      }
+    }),
+
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths()]
 });
